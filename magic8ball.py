529bbbd422dc9073918f3529f26cc19483ab4e95
s203 <s203@AI241LAB0203M22.local>
2017-11-01 09:36:11 -0700

    Added Magic 8 Ball program

---
+++ magic8ball.py
@@ -0,0 +1,23 @@
+import random
+print("This world is full of possibilities. User, give me your name.\nI shall tell you your fate.")
+name=str(input())
+number=random.randint(1, 9)
+if number==1:
+    print("\nYou will go insane and become a rabid Jake Pauler.\n")
+elif number==2:
+    print("\nYou will be awestruck at the end of Undertale 2 when it turns out Ness was Link from Sonic: Odyssey all along.\n")
+elif number==3:
+    print("\nYou will become the number one killer of people who believe there are more than 3 Fire Emblem™ games.\nThis is the best fate possible.\n")
+elif number==4:
+    print("\nYou will one day legitimately think only people with high intelligence can understand 'Rick and Morty.'\nWhy are you like this?\n")
+elif number==5:
+    print("\nYou will buy a virtual reality headset but then game developers \nwill not make games for it and you will be left with a useless gadget.\nRip in pasta.\n")
+elif number==6:
+    print("\nYou will drink the water Alex Jones is worried about and reverse your sexuality.\n")
+elif number==7:
+    print("\nYou will punch someone for liking Meghan Trainor with no legal repercussions. \nThis is the 2nd best outcome.\n")
+elif number==8:
+    print("\nYou will encounter Lady Evil in South Carolina.\n")
+elif number==9:
+    print("\nYou will get cancer. Not the disease, I just mean you'll buy the Emoji Movie.\n")
+print("\nThis is your fate "+name+". Do not try to change it. \nOnly catastrophe will follow.\n")
