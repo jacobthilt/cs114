214da0ef953d7d00822c636bdb91398fc1b026dc
s203 <s203@2024-01-203-22.local>
2017-11-29 12:03:39 -0800

    Week 9 submissions

--- space3.py
+++ space3.py
@@ -1,33 +1,32 @@
 import random
-
+magic=50
+attack=50
 
 input("\nA Todd dummy appears! Press enter to start.\n")
 
-def action(magic, attack, decision):
+def action(Fmagic, Fattack, decision):
     if decision=='m':
-        #while magic>0 and decision=='m':
+        #while Fmagic>0 and decision=='m':
         randomNumber=random.randint(1, 25)
-        print("You use ",randomNumber,"magic points on yourself.")
-        magic= magic-randomNumber
-        if magic<=0:
-            magic=0
-        print("\nAttack: ",attack,"\nMagic: ",magic,"\n")
+        print("You use ",randomNumber,"Fmagic points on yourself.")
+        Fmagic= Fmagic-randomNumber
+        if Fmagic<=0:
+            Fmagic=0
+        print("\nAttack: ",Fattack,"\nMagic: ",Fmagic,"\n")
     elif decision=='a':
-        #while attack>0 and decision=='a':
+        #while Fattack>0 and decision=='a':
         randomNumber=random.randint(1, 25)
         print("You attack the dummy.")
-        attack=attack-randomNumber
+        Fattack=Fattack-randomNumber
         print("You lose",randomNumber,"attack points.")
-        if attack<=0:
-            attack=0
-        print("\nAttack: ",attack,"\nMagic: ",magic,"\n")
+        if Fattack<=0:
+            Fattack=0
+        print("\nAttack: ",Fattack,"\nMagic: ",Fmagic,"\n")
     else:
         print("\nThat choice is invalid. \nAttack: 0\nMagic:0\n\nYou have died.")
         quit()
 
 def main():
-    magic=50
-    attack=50
     while magic>0 or attack>0:
         choice=str(input("Enter 'm' to use magic or 'a' to attack: "))
         action(magic, attack, choice)
